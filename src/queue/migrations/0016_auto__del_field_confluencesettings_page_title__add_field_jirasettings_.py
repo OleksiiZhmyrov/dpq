415c1ac07384c06f5ae7ecc1e9d32abdcbde91b6
Oleksii Zhmyrov <zhmyrov@gmail.com>
2014-03-30 23:04:00 +0300

    Update deskcheck status handling

---
+++ src/queue/migrations/0016_auto__del_field_confluencesettings_page_title__add_field_jirasettings_.py
@@ -0,0 +1,237 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Deleting field 'ConfluenceSettings.page_title'
+        db.delete_column(u'queue_confluencesettings', 'page_title')
+
+        # Adding field 'JiraSettings.custom_field_onshore_ba'
+        db.add_column('queue_jirasettings', 'custom_field_onshore_ba',
+                      self.gf('django.db.models.fields.CharField')(default='', max_length=32, null=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Adding field 'ConfluenceSettings.page_title'
+        db.add_column(u'queue_confluencesettings', 'page_title',
+                      self.gf('django.db.models.fields.CharField')(max_length=64, null=True, blank=True),
+                      keep_default=False)
+
+        # Deleting field 'JiraSettings.custom_field_onshore_ba'
+        db.delete_column('queue_jirasettings', 'custom_field_onshore_ba')
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'queue.boardsticker': {
+            'Meta': {'object_name': 'BoardSticker'},
+            'created_by': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['queue.CustomUserRecord']"}),
+            'creation_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_completed': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_modified': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'modification_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'retroBoard': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['queue.RetroBoard']"}),
+            'status_code': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '0', 'null': 'True', 'blank': 'True'}),
+            'summary': ('django.db.models.fields.CharField', [], {'max_length': '512', 'null': 'True', 'blank': 'True'}),
+            'type': ('django.db.models.fields.CharField', [], {'default': "'C'", 'max_length': '1'}),
+            'voters': ('django.db.models.fields.CharField', [], {'max_length': '512', 'null': 'True', 'blank': 'True'}),
+            'votes': ('django.db.models.fields.IntegerField', [], {'default': '0'})
+        },
+        'queue.branch': {
+            'Meta': {'object_name': 'Branch'},
+            'build_success': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'finish_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_build_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '32'}),
+            'start_date': ('django.db.models.fields.DateField', [], {'auto_now_add': 'True', 'blank': 'True'})
+        },
+        'queue.confluencesettings': {
+            'Meta': {'object_name': 'ConfluenceSettings'},
+            'SvG_page_title': ('django.db.models.fields.CharField', [], {'max_length': '64', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'login': ('django.db.models.fields.CharField', [], {'max_length': '20', 'null': 'True', 'blank': 'True'}),
+            'namespace': ('django.db.models.fields.CharField', [], {'max_length': '32', 'null': 'True', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '64', 'null': 'True', 'blank': 'True'}),
+            'url': ('django.db.models.fields.CharField', [], {'max_length': '128', 'null': 'True', 'blank': 'True'})
+        },
+        'queue.customuserrecord': {
+            'Meta': {'object_name': 'CustomUserRecord'},
+            'django_user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'role': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['queue.Role']"}),
+            'trump_cards': ('django.db.models.fields.IntegerField', [], {'default': '0', 'null': 'True', 'blank': 'True'})
+        },
+        'queue.deskcheckstatistic': {
+            'Meta': {'object_name': 'DeskCheckStatistic'},
+            'creation_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'failed_count': ('django.db.models.fields.FloatField', [], {}),
+            'failed_count_sp': ('django.db.models.fields.FloatField', [], {}),
+            'failed_percent': ('django.db.models.fields.FloatField', [], {}),
+            'failed_percent_sp': ('django.db.models.fields.FloatField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'other_count': ('django.db.models.fields.FloatField', [], {}),
+            'other_count_sp': ('django.db.models.fields.FloatField', [], {}),
+            'other_percent': ('django.db.models.fields.FloatField', [], {}),
+            'other_percent_sp': ('django.db.models.fields.FloatField', [], {}),
+            'passed_count': ('django.db.models.fields.FloatField', [], {}),
+            'passed_count_sp': ('django.db.models.fields.FloatField', [], {}),
+            'passed_percent': ('django.db.models.fields.FloatField', [], {}),
+            'passed_percent_sp': ('django.db.models.fields.FloatField', [], {}),
+            'ready_count': ('django.db.models.fields.FloatField', [], {}),
+            'ready_count_sp': ('django.db.models.fields.FloatField', [], {}),
+            'ready_percent': ('django.db.models.fields.FloatField', [], {}),
+            'ready_percent_sp': ('django.db.models.fields.FloatField', [], {}),
+            'total_count': ('django.db.models.fields.FloatField', [], {}),
+            'total_count_sp': ('django.db.models.fields.FloatField', [], {})
+        },
+        'queue.jirasettings': {
+            'Meta': {'object_name': 'JiraSettings'},
+            'browse_url': ('django.db.models.fields.CharField', [], {'max_length': '128', 'null': 'True', 'blank': 'True'}),
+            'custom_field_desk_check': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '32', 'null': 'True'}),
+            'custom_field_epic_name': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '32', 'null': 'True'}),
+            'custom_field_estimation_date': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '32', 'null': 'True'}),
+            'custom_field_onshore_ba': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '32', 'null': 'True'}),
+            'custom_field_story_points': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '32', 'null': 'True'}),
+            'custom_field_team': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '32', 'null': 'True'}),
+            'custom_field_tester': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '32', 'null': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'login': ('django.db.models.fields.CharField', [], {'max_length': '20', 'null': 'True', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '64', 'null': 'True', 'blank': 'True'}),
+            'project_name': ('django.db.models.fields.CharField', [], {'max_length': '64', 'null': 'True', 'blank': 'True'}),
+            'url': ('django.db.models.fields.CharField', [], {'max_length': '128', 'null': 'True', 'blank': 'True'})
+        },
+        'queue.outdatedjiraissue': {
+            'Meta': {'object_name': 'OutdatedJiraIssue'},
+            'assignee': ('django.db.models.fields.CharField', [], {'max_length': '64', 'null': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '1024', 'null': 'True', 'blank': 'True'}),
+            'epic': ('django.db.models.fields.CharField', [], {'max_length': '32', 'null': 'True', 'blank': 'True'}),
+            'estimation_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_deskcheck': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'key': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
+            'points': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'reporter': ('django.db.models.fields.CharField', [], {'max_length': '64', 'null': 'True', 'blank': 'True'}),
+            'summary': ('django.db.models.fields.CharField', [], {'max_length': '1024', 'null': 'True', 'blank': 'True'}),
+            'team': ('django.db.models.fields.CharField', [], {'max_length': '32', 'null': 'True', 'blank': 'True'}),
+            'tester': ('django.db.models.fields.CharField', [], {'max_length': '64', 'null': 'True', 'blank': 'True'})
+        },
+        'queue.queuerecord': {
+            'Meta': {'object_name': 'QueueRecord'},
+            'branch': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['queue.Branch']"}),
+            'creation_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'done_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'hidden': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'index': ('django.db.models.fields.PositiveIntegerField', [], {'unique': 'True'}),
+            'modified_date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
+            'push_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'queue_id': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '32'}),
+            'status': ('django.db.models.fields.CharField', [], {'default': "'W'", 'max_length': '1'}),
+            'story': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['queue.UserStory']"}),
+            'team': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['queue.Team']", 'null': 'True', 'blank': 'True'})
+        },
+        'queue.retroboard': {
+            'Meta': {'object_name': 'RetroBoard'},
+            'created_by': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['queue.CustomUserRecord']"}),
+            'creation_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'sprint': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['queue.Sprint']"}),
+            'team': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['queue.Team']"}),
+            'vote_limit': ('django.db.models.fields.IntegerField', [], {'default': '3'})
+        },
+        'queue.role': {
+            'Meta': {'object_name': 'Role'},
+            'can_create_records': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'can_modify_all_records': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'can_modify_own_records': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        'queue.sprint': {
+            'Meta': {'object_name': 'Sprint'},
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '128', 'null': 'True', 'blank': 'True'}),
+            'finish_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'motto': ('django.db.models.fields.CharField', [], {'max_length': '128', 'null': 'True', 'blank': 'True'}),
+            'number': ('django.db.models.fields.IntegerField', [], {}),
+            'start_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'})
+        },
+        'queue.statistics': {
+            'Meta': {'object_name': 'Statistics'},
+            'date': ('django.db.models.fields.DateField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'number_of_pushes': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'}),
+            'total_push_duration': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0'})
+        },
+        'queue.team': {
+            'Meta': {'object_name': 'Team'},
+            'css_icon': ('django.db.models.fields.CharField', [], {'max_length': '128', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'label': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '32'})
+        },
+        'queue.userstory': {
+            'Meta': {'object_name': 'UserStory'},
+            'assignee': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'key': ('django.db.models.fields.CharField', [], {'max_length': '16'}),
+            'last_sync': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'summary': ('django.db.models.fields.CharField', [], {'max_length': '256'}),
+            'tester': ('django.db.models.fields.CharField', [], {'max_length': '256'})
+        },
+        'queue.usertoretroboardconnector': {
+            'Meta': {'object_name': 'UserToRetroBoardConnector'},
+            'custom_user_record': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['queue.CustomUserRecord']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'retroBoard': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['queue.RetroBoard']"}),
+            'votes': ('django.db.models.fields.IntegerField', [], {'default': '0'})
+        }
+    }
+
+    complete_apps = ['queue']
